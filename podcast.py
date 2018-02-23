6a05bbb67b42d2c7af1a7580a59743aeced85729
Aykan F <aykanf@MacBook-Pro.local>
2018-02-22 18:01:12 -0800

    Added updates to podcast scraper.

--- podcast.py
+++ podcast.py
@@ -1,30 +1,40 @@
-from bs4 import BeautifulSoup 
-import requests
-import re
+'''Python program to scrape UC San Diego's podcasts for a quarter. Created by Aykan Fonseca.'''
+
+# TODO:
+# 1. Update to parse any quarter. Currently only parses the current quarter.
+
+# Builtins
 import time
 
-def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
-    return [int(text) if text.isdigit() else text.lower() for text in re.split(_nsre, s[0])]
+# Pip install packages.
+from bs4 import BeautifulSoup
+from firebase import firebase
+import requests
 
-def main():
-    start = time.time()
+# Used to convert between shortened codes.
+quarter_mapping = {'Fall':'FA', 'Winter':'WI', 'Spring':'SP', 'Summer Med School':'SU', 'Summer Session 1':'S1', 'Summer Session 2':'S2', 'Summer Session 3':'S3', 'Summer':'SA'}
+podcast_url = "https://podcast.ucsd.edu"
 
-    podcast_url = "https://podcast.ucsd.edu"
 
+def parse_data():
     post = requests.get(podcast_url)
     soup = BeautifulSoup(post.content, 'lxml')
 
-    # A list of lists where each list is contains: title of course, professor, authentication, and podcast link.
+    # A list of dictionaries where each list is contains: title of course, professor, authentication, and podcast link.
     podcasts = []
+
+    quarter_year = soup.find('div', {'class': 'quarter'}).h2.span.text.split(' ')[::2]
+
+    quarter = quarter_mapping[quarter_year[0]] + quarter_year[1][-2:]
 
     table = soup.find('div', {'class': 'quarter'}).findAll('tr')
 
     for item in table:
-        sub = []
+        sub = {}
 
         # # Append the Class name & Professor.
-        sub.append(item.findAll('td')[0].text.strip().partition('-')[0].strip())
-        sub.append(item.findAll('td')[1].text.strip())
+        sub['class'] = item.findAll('td')[0].text.strip().partition('-')[0].strip()
+        sub['professor'] = item.findAll('td')[1].text.strip()
 
         # True / False value if log in authentication is required. Assume false. 
         authentication = False
@@ -33,27 +43,57 @@ def main():
             authentication = True
         
         # Append the authentication.
-        sub.append(authentication)
+        sub['authentication'] = authentication
 
         # Podcast link.
-        sub.append(item.findAll('td')[2].findAll('a')[0]['href'][:-4])
+        sub['link'] = item.findAll('td')[2].findAll('a')[0]['href'][:-4]
 
         podcasts.append(sub)
 
-    # Natural sort the classes based on first item in list for each list in list of lists. 
-    podcasts.sort(key=natural_sort_key)
+    return podcasts, quarter
 
-    # Print data.
-    for i in podcasts:
-        print i[0]
-        print i[1]
-        print i[2]
-        print i[3]
-        print "\n"
 
-    end = time.time()
+def update_db(podcasts, quarter):
+    database = firebase.FirebaseApplication("https://schedule-of-classes-8b222.firebaseio.com/")
 
-    print("\nTime taken: " + str(end - start))
+    for item in podcasts:
+        # If the podcast is for two courses, just pick the first one. TODO: NEED TO UPDATE.
+        if item['class'].split(' ') > 2:
+            item['class'] = ' '.join(item['class'].split(' ')[:2])
+        
+        path = "/quarter/" + quarter + "/" + str(item['class']) + "/"
+
+        # Updates node when node exists. If not, don't add because we won't use.
+        if (database.get(path, None) != None): 
+            database.put(path, 'podcast', {'authentication': item['authentication'], 'link': item['link']})
+
+
+def reset_db():
+    """ Deletes data to firebase."""
+
+    print("Wiping information in database.")
+
+    database = firebase.FirebaseApplication("https://schedule-of-classes-8b222.firebaseio.com/")
+
+    database.delete('/quarter', None)
+
+
+def main():
+    '''The main function.'''
+    print(sys.version)
+
+    reset = False
+
+    if (reset):
+        reset_db()
+
+    start = time.time()
+
+    podcasts, quarter = parse_data()
+
+    update_db(podcasts, quarter)
+
+    print("\nTime taken: " + str(time.time() - start))
 
 if __name__ == '__main__':
     main()
